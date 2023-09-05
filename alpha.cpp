6f5ed1d02a4ebdd5334043c34897f7e4e5eaab0a
Abhishek Kumar Karn <66510470+karnabhishek@users.noreply.github.com>
2023-09-06 00:03:26 +0530

    Add files via upload

---
+++ alpha.cpp
@@ -0,0 +1,20 @@
+
+
+char ch,s;
+  int b;
+  ch='a';
+  cin>>ch;
+  if(ch>=z){
+  cout<<"This is lower case";
+    }
+    s='A';
+  cin>>s;
+  else if (s<=Z){
+    cout<<"This is uper case";
+  }
+      b=0;
+  cin>>b;
+  else if (b<=9){
+    cout<<"this is number";
+  }
+      }
